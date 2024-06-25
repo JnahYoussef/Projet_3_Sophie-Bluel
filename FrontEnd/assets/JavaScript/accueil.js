5fb296569e5c7fdd605c9434138e5032425b329c
Youssef Jnah <youssefj86@gmail.com>
2024-06-25 17:54:31 +0200

    Récuperer et Afficher le tableau des catégories

--- FrontEnd/assets/JavaScript/accueil.js
+++ FrontEnd/assets/JavaScript/accueil.js
@@ -1,30 +1,90 @@
 // ****** Variables ******//
 
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
 
-//** function qui retourne le tableau des works **/:
+//** function qui retourne le tableau des projets **/:
 
-async function getWorks () {
+async function getProjects () {
     const response = await  fetch ("http://localhost:5678/api/works");
     return await response.json()
    
 }
-getWorks();
+getProjects();
 
-//** Affichage des works dans le dom **/
+//** Affichage des projets dans le DOM **/
 
-async function affichageWorks() {
-    const arrayWorks = await getWorks ()
-    arrayWorks.forEach(element => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
-        img.src = element.imageUrl;
-        figcaption.innerText = element.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
+async function displayProjects() {
+    const projects = await getProjects ()
+    projects.forEach(project => {
+        createProjects(project)
     });
 }
-affichageWorks();
+displayProjects();
+
+function createProjects(project){
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    img.src = project.imageUrl;
+    figcaption.innerText = project.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+}
+
+//*** Affichage des boutons par catégories ***//
+
+//récupérer le tableau des catégories 
+
+async function getCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+}
+
+//Afficher le tableau des catégories
+async function displayCategoriesButtons(){
+    const categories = await getCategories();
+    categories.unshift({ id: 0, name: "Tous" }); // Ajoute une catégorie "Tous" pour permettre l'affichage de tous les travaux.
+    categories.forEach(category => {
+        const btn = document.createElement("button");
+        btn.textContent = category.name;
+        btn.id = category.id;
+        if (category.id === 0) {
+            btn.classList.add("selected"); // Ajouter la classe "selected" au bouton 'Tous'
+        }
+        filters.appendChild(btn);
+    });
+}
+displayCategoriesButtons();
+
+
+//*** Filtrer par catégorie au clic ***//
+
+async function filterCategory () {
+    const allProjects = await getProjects();
+    const buttons = document.querySelectorAll(".filters button");
+    
+    buttons.forEach(button => {
+        button.addEventListener ("click", (event) =>{
+            btnId = event.target.id;
+            gallery.innerHTML = ""; //pour vider la gallery et afficher que la sélection, sinon tous s'affiche en plus de la sélection en bas
+
+            buttons.forEach(btn => btn.classList.remove('selected')); // Retirer la classe "selected" de tous les boutons
+            event.target.classList.add('selected'); // Ajouter la classe "selected" au bouton cliqué
+            
+            if(btnId !== "0"){
+                const allProjectsSort = allProjects.filter(project =>{
+                    return project.categoryId == btnId
+                });
+                allProjectsSort.forEach(project => {
+                    createProjects(project)
+                });
+            }else{
+                displayProjects();
+            }
+        });
+    });
+}
+filterCategory();
